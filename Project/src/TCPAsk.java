905fefc5e40f57e5b28155c1922a39159e4b4fe0
plut092 <johan.m.engstrom92@gmail.com>
2020-02-02 23:50:15 +0100

    First skeleton added

--- Project/src/TCPAsk.java
+++ Project/src/TCPAsk.java
@@ -1,2 +1,20 @@
+import java.io.*;
+import java.net.*;
+
+
 public class TCPAsk {
+    public static void main(String[] args) throws Exception {
+        askServer("time.nist.gov", 13);
+    }
+
+    public static String askServer(String hostname, int port, String ToServer) throws  IOException {
+        String outFromServer = null;
+
+        return outFromServer;
+    }
+    public static String askServer(String hostname, int port) throws  IOException{
+        String outFromServer = null;
+
+        return outFromServer;
+    }
 }
